609264598582e16f09335f64eb6f2c05d2de92ad
Adriano J. Holanda <ajholanda@gmail.com>
2015-05-13 14:26:09 -0300

    added more homework

--- function-values.go
+++ function-values.go
@@ -6,9 +6,9 @@ import (
 )
 
 func main() {
-	hypot := func(x, y float64) float64 {
+	hipot := func(x, y float64) float64 {
 		return math.Sqrt(x*x + y*y)
 	}
 
-	fmt.Println(hypot(3, 4))
+	fmt.Println(hipot(3, 4))
 }
